1d71960ad4ff117c41cf9a10214b992ca0c60ba1
Josias Daniel de Souza Moreira <josiasdanielmoreirasouza@gmail.com>
2025-02-15 14:29:30 -0300

    the next is the chalangers

---
+++ exercicios/ex005.py
@@ -0,0 +1,8 @@
+nome = input('Nome:')
+print('Olá mundo e Sr.{:20}.'.format(nome))
+
+n1 = int(input('Valor:'))
+n2 = int(input('Valor 2'))
+s = n1 + n2
+
+print('A soma de {} e {} é igual: {} ' .format(n1,n2,s))
