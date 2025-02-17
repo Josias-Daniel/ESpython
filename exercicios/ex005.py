ce92ac006a8072954634d1cf4acb9e507d397816
Josias Daniel de Souza Moreira <josiasdanielmoreirasouza@gmail.com>
2025-02-17 19:33:41 -0300

    Booooa

--- exercicios/ex005.py
+++ exercicios/ex005.py
@@ -6,3 +6,8 @@ n2 = int(input('Valor 2'))
 s = n1 + n2
 
 print('A soma de {} e {} é igual: {} ' .format(n1,n2,s))
+
+N = int(input ('digite um número'))
+Na = N - 1
+Ns = N + 1
+print('O número é {}, seu antecessor é {} e seu sucessor é {}.'.format(N, Na, Ns))
