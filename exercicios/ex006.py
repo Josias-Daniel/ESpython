9cd83b6eeab26219019c7af4dad061f5ef0f6bc4
Josias Daniel de Souza Moreira <josiasdanielmoreirasouza@gmail.com>
2025-02-18 11:26:21 -0300

    bora sem parar nem um dia

---
+++ exercicios/ex006.py
@@ -0,0 +1,2 @@
+n = int(input('Escreva um número'))
+print('O dobro de {} é {}, A raiz quadrada de {} é {}, e o triplo de {} é {}'.format(n,(n * 2),n , (n ** (1/2)), n, (n * 3)))
---
+++ exercicios/ex007.py
@@ -0,0 +1,3 @@
+n = float(input('Escreva um número '))
+n2 = float(input('Escreva outro número '))
+print('A média entre {} e {} é igual a {}'.format(n, n2, ((n+n2)/2)))
