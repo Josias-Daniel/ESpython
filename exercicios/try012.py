d14c716c4a10fa503c58a78d1ff85f7764c75b0d
Josias Daniel de Souza Moreira <josiasdanielmoreirasouza@gmail.com>
2025-03-31 11:44:02 -0300

    almost finished try 12

---
+++ exercicios/try012.py
@@ -0,0 +1,15 @@
+n = str(input('Digite um nome: '))
+
+if n == 'Josias':
+    print('Seu nome é bem interessante')
+
+elif n == 'maria' or n == 'joão' or n == 'josé':
+    print('Seu nome é bem popular.')
+
+elif n == 'Rosi' or n =='Larissa' or n == 'Julia':
+    print('lindo nome feminino.')
+
+else:
+    print('Seu nome é bem normal.')
+
+print('tenha um bom dia.')
