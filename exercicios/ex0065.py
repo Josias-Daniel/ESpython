dca4bf5bbe1a71c132ce0d7cbe7e3832c3e7962b
Josias Daniel de Souza Moreira <josiasdanielmoreirasouza@gmail.com>
2025-04-28 18:01:44 -0300

    fineshed 65.py

--- exercicios/ex0065.py
+++ exercicios/ex0065.py
@@ -3,13 +3,19 @@ rsn = ''
 r = input('Diga um número: ')
 mr = r   
 mn = r 
-rsn = str(input('Quer continuar: '))
-while rsn != 'N':
+rsn = str(input('Quer continuar: ')).upper().strip()[0]
+
+while rsn in  'Ss':
     r = input('Diga um número: ')
     if r > mr:
         mr = r
-    elif r < mn:
+    if r < mn:
         mn = r
-    rsn = str(input('Quer continuar: '))
+    rsn = str(input('Quer continuar: ')).upper().strip()[0]
+    if rsn != 'S':
+        if rsn != 'N':
+            print('Algo errado')
+            rsn = 'S'
+
 
 print('Número maior {}, número menor {}'.format(mr,mn))
