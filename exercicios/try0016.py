a6bb0233e807b9a0109cd18d3b35365570ee36dc
Josias Daniel de Souza Moreira <josiasdanielmoreirasouza@gmail.com>
2025-05-11 15:16:07 -0300

    try0016.py

---
+++ exercicios/try0016.py
@@ -0,0 +1,10 @@
+personas = ('Aynokoji'  , 'Calmo', 'Barou', 'Verde')
+roxos = ('GKblack', 'Sung', 27, 'Shadow')
+legal = personas + roxos
+c = 0
+while True:
+    print((legal[c]),c)
+    c = c + 1
+    if c == 7:
+        print('FIM')
+        break
