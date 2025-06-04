1d2510c04adc3edaba672ae119e2ed640e533600
Josias Daniel de Souza Moreira <josiasdanielmoreirasouza@gmail.com>
2025-06-04 19:37:44 -0300

    finished try0019.py

--- exercicios/try0019.py
+++ exercicios/try0019.py
@@ -1,5 +1,28 @@
 dados = {'nome':'Josias','idade':15,'sexo':'M'}
+dados2 = {'nome':'Julia','idade':'15','sexo':'F',}
+nomes = []
+nomes.append(dados)
+nomes.append(dados2)
 print(dados.values())
 print(dados.keys())
-for k, v in dados.items():
+for k, v in dados2.items():
     print(f'O {k} é {v}') 
+print(nomes)
+print(nomes[1]['nome'])
+
+print('=' * 30)
+c = 0
+br = []
+es = {}
+for c in range(0, 2):
+    es['n'] = str(input('Nome do estado: '))
+    es['sigla'] = str(input('Sigla: '))
+    br.append(es.copy())
+for e in br:
+    for v in e.values(): 
+        c = c + 1
+        if c % 2 == 0:
+            print(f'{v}', end=':    ')
+        elif c % 2 != 0:
+            print(v) 
+        
