3602506212441c1511c3c29463804c0c4ada06d0
Josias Daniel de Souza Moreira <josiasdanielmoreirasouza@gmail.com>
2025-06-17 18:44:06 -0300

    finished ex0097.py

---
+++ exercicios/ex0096.py
@@ -0,0 +1,8 @@
+def x(a, b):
+    r = (a * b)
+    print(f'A área de um terreno {a} X {b} é de {r}m/_2')
+
+
+a = float(input('Largura [m]: '))
+b = float(input('Comprimento [m]: '))
+x(a, b)
---
+++ exercicios/ex0097.py
@@ -0,0 +1,8 @@
+def m(men):
+    tam = len(men)
+    print('=' * (tam + 6))
+    print(f'   {men}')
+    print('=' * (tam + 6))
+
+
+m('JOSIAS DANIELjjjjjjjjjjjjjjjjjjjjjjjj')
